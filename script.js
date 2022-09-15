39723114a350a5fe968e576b119a9985551ce0ad
ricomortus <38750470+ricomortus@users.noreply.github.com>
2022-09-15 08:11:55 +0300

    add js to make grid dynamic with different # of rows and columns.

--- script.js
+++ script.js
@@ -8,21 +8,29 @@ for (v = 1; v <= userColumn; v++) {
     const divContainer = document.createElement('div');
     divContainer.setAttribute("class", `col-${v}-container`);
     gridContainer.appendChild(divContainer);
-
     let tempColContainer = document.querySelector(`.col-${v}-container`);
     tempColContainer.style.cssText = `grid-column-start: ${v}`;
-    
     // Creates rows
     for (i = 1; i <= userRow; i++) {
         const div = document.createElement('div');
         div.setAttribute("class", `col-${v}`);
         div.setAttribute("id", `box${i}`);
-        div.style.cssText = 'height: 38px; width: 38px; border: 1px solid black; margin: 0';
         tempColContainer.appendChild(div);
-
-
-        div.addEventListener ('mouseenter', () => {div.style.cssText = 'background-color: grey'})
+        //style individual grid squares 
+        div.style.cssText = 
+        `height: ${(640 / userColumn) - 2}px; 
+        width: ${(640 / userColumn) - 2}px; 
+        border: 1px solid black; 
+        margin: 0;`;
+        //Changes colour when hovered + retain previous CSS
+        div.addEventListener ('mouseenter', () => {
+            div.style.cssText = 
+            `height: ${(640 / userColumn) - 2}px; 
+            width: ${(640 / userColumn) - 2}px; 
+            border: 1px solid black; 
+            margin: 0; 
+            background-color: black`
+        })
     }
-}
-
+};
 
