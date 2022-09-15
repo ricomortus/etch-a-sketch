27af4e0d5c7f89f63eab07cb04db3570892b0271
ricomortus <38750470+ricomortus@users.noreply.github.com>
2022-09-15 07:38:26 +0300

    add css style to grid squares via js.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const gridContainer = document.querySelector('.grid-container');
 
-for (v = 1; v <= 16; v++) {
+let userColumn = 16;
+let userRow = 16;
+
+for (v = 1; v <= userColumn; v++) {
+    // Creates columns
     const divContainer = document.createElement('div');
     divContainer.setAttribute("class", `col-${v}-container`);
     gridContainer.appendChild(divContainer);
@@ -8,11 +12,14 @@ for (v = 1; v <= 16; v++) {
     let tempColContainer = document.querySelector(`.col-${v}-container`);
     tempColContainer.style.cssText = `grid-column-start: ${v}`;
     
-    for (i = 1; i <= 16; i++) {
+    // Creates rows
+    for (i = 1; i <= userRow; i++) {
         const div = document.createElement('div');
         div.setAttribute("class", `col-${v}`);
         div.setAttribute("id", `box${i}`);
+        div.style.cssText = 'height: 38px; width: 38px; border: 1px solid black; margin: 0';
         tempColContainer.appendChild(div);
+
 
         div.addEventListener ('mouseenter', () => {div.style.cssText = 'background-color: grey'})
     }
